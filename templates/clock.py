7f9a29893b2e7d41e275713fd6ca4fd4360128e2
Ryan Sutton <git@ryanmsutton.com>
2024-08-25 13:05:16 -0400

    refoctor clock

--- templates/clock.py
+++ templates/clock.py
@@ -44,13 +44,14 @@ class clock(base):
         x = self.clock_xoffset
         for tz in self.timezones:
             t = datetime.now(ZoneInfo(tz)).strftime("%H %M")
-            label = tz_label_map.get(tz, tz)
-            label_offset = int((42-(len(label)*6))/2)
-            label_offset = label_offset if label_offset > 0 else 0
-            self.update_message(label.upper(), (x+label_offset, 15), fgcolor=self.label_color, bgcolor=self.bgcolor)
+            if self.show_label:
+                label = tz_label_map.get(tz, tz)
+                label_offset = int((42-(len(label)*6))/2)
+                label_offset = label_offset if label_offset > 0 else 0
+                self.update_message(label.upper(), (x+label_offset, 15), fgcolor=self.label_color, bgcolor=self.bgcolor)
             for c in t:
                 if c.isdigit():
-                    self.draw_7seg_digit(c, x,bgcolor=self.bgcolor)
+                    self.draw_7seg_digit(c, x, y_offset=self.clock_yoffset, bgcolor=self.bgcolor)
                 else:
                     self.draw_box((x+2, self.clock_yoffset+2), 3, 2, self.fgcolor)
                     self.draw_box((x+2, self.clock_yoffset+10), 3, 2, self.fgcolor)
