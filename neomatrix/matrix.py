d21288664a0ba28cfbdcd4be15cee47db4b2b9ab
Ryan Sutton <git@ryanmsutton.com>
2025-11-29 21:08:54 -0500

    Ball PoC

--- neomatrix/matrix.py
+++ neomatrix/matrix.py
@@ -39,6 +39,7 @@ class matrix():
             self.np[address] = color
         elif self.mode == "SPI" or self.mode == "PYSPI":
             port = self.yoffset + ( self.xoffset * 1 )
+            print(port, address)
             data_to_send = int(15).to_bytes(1,"big") + ((port << 9) | address).to_bytes(2,"big") + color[1:2] + color[0:1] + color[2:3]
             if self.mode == "SPI":
                 self.cs(0)
